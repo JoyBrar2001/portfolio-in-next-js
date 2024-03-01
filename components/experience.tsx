c0ff5fb9cb75f5289ff7b866b1dbf877cc629f90
Joy Brar <joybrar2001@gmail.com>
2024-03-02 05:27:30 +0530

    Experience section done

---
+++ components/experience.tsx
@@ -0,0 +1,44 @@
+'use client'
+
+import React from "react";
+import { experiencesData } from "@/lib/data";
+import SectionHeading from "./section-heading";
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+
+export default function Experience() {
+  return (
+    <section id="experience" className="scroll-mt-28 mb-28 sm:mb-40">
+      <SectionHeading>My Experience</SectionHeading>
+      <VerticalTimeline lineColor="" animate={true}>
+        {experiencesData.map((experience, index) => (
+          <React.Fragment key={index}>
+            <VerticalTimelineElement
+              visible={true}
+              contentStyle={{
+                background: "#f3f4f6",
+                boxShadow: "none",
+                border: "1px solid rgba(0, 0, 0, 0.05)",
+                textAlign: "left",
+                padding: "1.3rem 2rem",
+              }}
+              contentArrowStyle={{
+                borderRight: "0.4rem solid #9ca3af",
+              }}
+              date={experience.date}
+              icon={experience.icon}
+              iconStyle={{
+                background: "white",
+                fontSize: "1.5rem",
+              }}
+            >
+              <h3 className="font-semibold capitalize">{experience.title}</h3>
+              <p className="font-normal !mt-0">{experience.location}</p>
+              <p className="!mt-1 !font-normal text-gray-700">{experience.description}</p>
+            </VerticalTimelineElement>
+          </React.Fragment>
+        ))}
+      </VerticalTimeline>
+    </section>
+  )
+}
--- components/skills.tsx
+++ components/skills.tsx
@@ -20,7 +20,7 @@ const fadeInAnimationsVariants = {
 
 export default function Skills() {
   return (
-    <section className="mb-28 max-w-[53rem] scroll-mt-28 text-center sm:mb-40">
+    <section id="skills" className="mb-28 max-w-[53rem] scroll-mt-28 text-center sm:mb-40">
       <SectionHeading>My Skills</SectionHeading>
       <ul className="flex flex-wrap justify-center gap-2 text-lg text-gray-800">
         {skillsData.map((skill, index) => (
