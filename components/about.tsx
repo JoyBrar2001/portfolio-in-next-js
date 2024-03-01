6b31a60c086f85b2f19de471cc2bf0be697dfa5a
Joy Brar <joybrar2001@gmail.com>
2024-03-01 19:44:31 +0530

    Finished the projects section

--- components/about.tsx
+++ components/about.tsx
@@ -13,7 +13,7 @@ export default function About() {
     >
       <SectionHeading>About Me</SectionHeading>
       <p className="mb-3">
-      Hey there! I'm Joy, a third-year{" "} 
+      Hey there! I&apos;m Joy, a third-year{" "} 
       <span className="font-medium">Computer Science and Engineering</span> 
       {" "}student at{" "}
       <span className="italic">Manipal University Jaipur</span>
@@ -21,16 +21,16 @@ export default function About() {
       <span className="font-medium">React.js, Next.js, and React Native</span>
       , as well as backend expertise using{" "}
       <span className="font-medium">Node.js and MongoDB</span>
-      , I've contributed to a variety of projects ranging from website revamps to cross-platform app development. From leading web operations as a{" "}
+      , I&apos;ve contributed to a variety of projects ranging from website revamps to cross-platform app development. From leading web operations as a{" "}
       <span className="font-medium">Web Master</span>
       {" "}for{" "}
       <span className="font-medium">MUJ ACM</span>
       {" "}
       (<span className="italic underline">Association for Computing Machinery</span>)
-      {" "} to my stint as a Software Engineering Intern at Clarient Systems Corporation, I've gained valuable hands-on experience in building robust, user-centric solutions.
+      {" "} to my stint as a Software Engineering Intern at Clarient Systems Corporation, I&apos;ve gained valuable hands-on experience in building robust, user-centric solutions.
       </p>
       <p>
-       I thrive on innovation and am dedicated to leveraging technology to create impactful solutions that make a difference. Let's connect and create something amazing together!
+       I thrive on innovation and am dedicated to leveraging technology to create impactful solutions that make a difference. Let&apos;s connect and create something amazing together!
       </p>
     </motion.section>
   )
