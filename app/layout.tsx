17f94479dfd4b996cc83f11b366a9596e6ee4d77
Joy Brar <joybrar2001@gmail.com>
2024-07-09 01:53:01 +0530

    Speed Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import "./globals.css";
 import Header from "@/components/header";
 import ActiveSectionContextProvider from "@/context/active-section-context";
@@ -32,6 +33,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
           </ActiveSectionContextProvider>
           <ThemeSwitch />
         </ThemeContextProvider>
+        <SpeedInsights />
         <Analytics />
       </body>
     </html>
